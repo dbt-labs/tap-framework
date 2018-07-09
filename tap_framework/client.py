60e63e4f99e87f55d96eb1d0f772628c96f8afc6
Connor McArthur <cmcarthur@rjmetrics.com>
2018-07-09 10:40:37 -0400

    fix backoff

--- tap_framework/client.py
+++ tap_framework/client.py
@@ -13,7 +13,7 @@ class BaseClient:
     def get_authorization(self):
         raise RuntimeError("get_authorization not implemented!")
 
-    def make_request(self, url, method, base_backoff=30,
+    def make_request(self, url, method, base_backoff=45,
                      params=None, body=None):
         auth = self.get_authorization()
 
@@ -33,7 +33,7 @@ class BaseClient:
             LOGGER.info('Got a 429, sleeping for {} seconds and trying again'
                         .format(base_backoff))
             time.sleep(base_backoff)
-            self.make_request(url, method, base_backoff * 2, params, body)
+            return self.make_request(url, method, base_backoff * 2, params, body)
 
         if response.status_code != 200:
             raise RuntimeError(response.text)
