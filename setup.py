a4c6c665e2bde61f81d7dc118d7af74331fcfeb4
Connor McArthur <connor@fishtownanalytics.com>
2018-08-15 09:52:59 -0400

    bump 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 import os.path
 
 setup(name='tap-framework',
-      version='0.0.1',
+      version='0.0.2',
       description='Framework for building Singer.io taps',
       author='Fishtown Analytics',
       url='http://fishtownanalytics.com',
